9c0bfd6035b383e36dac0a2e0700b9571398e899
zMed-tech <badache.amn@gmail.com>
2021-06-09 22:21:38 +0100

    feat: code of router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,52 @@ const routes = [
     name: "Home",
     component: Home,
   },
+
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login.vue"),
+    meta: {
+      guest: true,
+    },
+  },
+
+  {
+    path: "/user",
+    name: "User",
+    component: () => import("@/views/User.vue"),
+    meta: {
+      auth: true,
+    },
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.auth)) {
+    if (localStorage.getItem("key")) {
+      next();
+    } else {
+      next({
+        name: "Login",
+      });
+    }
+  } else if (to.matched.some((record) => record.meta.guest)) {
+    if (localStorage.getItem("key")) {
+      next({
+        name: "User",
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
