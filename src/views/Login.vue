f985ac2bf565cecad8aa80b79058ea7f77a6a203
zMed-tech <badache.amn@gmail.com>
2021-06-09 22:21:49 +0100

    feat: Login Page

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    <Authenticate />
+  </div>
+</template>
+
+<script>
+import Authenticate from "@/components/Authenticate.vue";
+
+export default {
+  name: "Login",
+  components: {
+    Authenticate,
+  },
+};
+</script>
