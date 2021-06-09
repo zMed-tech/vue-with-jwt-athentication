9cba1adf9afc4cb99456076e46a8e55117f2822e
zMed-tech <badache.amn@gmail.com>
2021-06-09 22:21:13 +0100

    feat: create authenticate component that contain both of signUp and signIn

--- src/components/Authenticate.vue
+++ src/components/Authenticate.vue
@@ -1,9 +1,53 @@
 <template>
-  <div id="authenticate"></div>
+  <div id="authenticate">
+    <transition name="slide" mode="out-in">
+      <component :is="comp" @swit="swit" />
+    </transition>
+  </div>
 </template>
 
 <script>
-export default {};
+import SignUp from "@/components/SignUp.vue";
+import SignIn from "@/components/SignIn.vue";
+
+export default {
+  name: "Authentication",
+  components: { SignUp, SignIn },
+
+  data() {
+    return {
+      comp: SignIn,
+    };
+  },
+
+  methods: {
+    swit(value) {
+      if (value == "signUp") {
+        this.comp = SignUp;
+      } else {
+        this.comp = SignIn;
+      }
+    },
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+#authenticate {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all ease-in-out 1s;
+}
+
+.slide-enter,
+.slide-leave-to {
+  transform: translateX(120px);
+  opacity: 0;
+}
+</style>
